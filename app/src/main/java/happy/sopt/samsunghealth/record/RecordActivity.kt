396a9843c7dd760eb3e54f6993fea6d4b49a7bb8
mym0404 <mym0404@gmail.com>
2022-05-20 00:58:39 +0900

    RecordActivity에서 EditText 이벤트 처리해서 UI 로직 구현

--- app/src/main/java/happy/sopt/samsunghealth/record/RecordActivity.kt
+++ app/src/main/java/happy/sopt/samsunghealth/record/RecordActivity.kt
@@ -1,7 +1,10 @@
 package happy.sopt.samsunghealth.record
 
 import android.os.Bundle
+import android.text.InputType
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isInvisible
+import androidx.core.widget.doOnTextChanged
 import happy.sopt.samsunghealth.databinding.ActivityRecordBinding
 
 class RecordActivity : AppCompatActivity() {
@@ -12,5 +15,28 @@ class RecordActivity : AppCompatActivity() {
 
         binding = ActivityRecordBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initWeightLogic()
+    }
+
+    private fun initWeightLogic() = binding.etInputWeightLeft.run {
+        inputType = InputType.TYPE_NUMBER_FLAG_SIGNED or InputType.TYPE_CLASS_NUMBER
+
+        // 포커스된 상태에 따라서 위 아래 텍스트가 보일지 말지를 결정
+        setOnFocusChangeListener { _, isFocused ->
+            binding.tvInputTop.isInvisible = isFocused
+            binding.tvInputBottom.isInvisible = isFocused
+        }
+
+        // 텍스트가 변했을 때 위 아래 텍스트들을 정수값을 파싱해서 변경
+        doOnTextChanged { text, _, _, _ ->
+            text!!.toString().toIntOrNull()?.let(::adjustLeftTopAndBottomHintTexts)
+        } // 가장 처음의 정수값을 반영
+        text!!.toString().toIntOrNull()?.let(::adjustLeftTopAndBottomHintTexts)
+    }
+
+    private fun adjustLeftTopAndBottomHintTexts(number: Int) {
+        binding.tvInputTop.text = (number - 1).toString()
+        binding.tvInputBottom.text = (number + 1).toString()
     }
 }
